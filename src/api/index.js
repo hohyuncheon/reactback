ee128d9304a2c25f2dc99e9e5ec8196999c5853e
hohyuncheon <lila230011@gmail.com>
2023-03-14 14:58:52 +0900

    chapter 22 db connect

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,9 @@
-const Router = require('koa-router');
-
-const posts = require('./posts');
+import Router from 'koa-router';
+import posts from './posts';
 
 const api = new Router();
 
-// api.get('/test', ctx=> {
-//     ctx.body = '성공';
-// })
+api.use('/posts', posts.routes());
 
-api.use('/posts', posts.routes())
-
-module.exports = api;
+// 라우터를 내보냅니다.
+export default api;
--- src/index.js
+++ src/index.js
@@ -1,47 +1,5 @@
-require('dotenv').config();
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-// const mongoose = require('mongoose');
-const { connect } = require('mongoose');
+// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화합니다
+/* eslint-disable no-global-assign */
 
-//비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const { PORT, MONGO_URI} = process.env;
-
-//기존
-// mongoose.connect(MONGO_URI).then(() => {
-//     console.log('mongoDb connect');
-// }).catch(e => {
-//     console.error(e)
-// })
-
-//connect not function version
-connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('MongoDB connected...');
-}).catch((err) => {
-    console.error(err);
-});
-
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-//라우터 설정
-router.use('/api', api.routes());
-
-app.use(bodyParser());
-
-app.use(router.routes()).use(router.allowedMethods());
-
-// app.listen(4000, () => {
-//     console.log('listening to port 4000');
-// });
-
-const port = PORT || 4000;
-app.listen(port, () => {
-    console.log('Listening to port %d', port);
-});
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
