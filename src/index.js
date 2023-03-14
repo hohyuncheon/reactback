a05a50f7a2b15eb966034b4d5a6c3f437b912985
hohyuncheon <lila230011@gmail.com>
2023-03-14 09:46:36 +0900

    chater 21 - mongoose connect success

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+// const mongoose = require('mongoose');
+const { connect } = require('mongoose');
 
 //비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const { PORT } = process.env;
+const { PORT, MONGO_URI} = process.env;
+
+//기존
+// mongoose.connect(MONGO_URI).then(() => {
+//     console.log('mongoDb connect');
+// }).catch(e => {
+//     console.error(e)
+// })
+
+//connect not function version
+connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('MongoDB connected...');
+}).catch((err) => {
+    console.error(err);
+});
 
 const api = require('./api');
 
